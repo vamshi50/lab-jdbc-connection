a4cd94f5222168fa3fa03179c858f041fd27c890
vamshi50 <telukuntlavamshi50@gmail.com>
2020-08-19 23:06:43 +0530

    Done

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,17 @@
 package controller;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Throwable, Exception, Exception{
 		
-	// Fill your code
-		
-	}
+
+		// Fill your code
+
+		Connection connection = ConnectionManager.getConnection();
+}
 }
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,36 @@
 package utility;
 
-
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
 	
-	
+	public static Connection getConnection() throws SQLException, IOException, ClassNotFoundException 
+	{
+		Properties properties = null;
+		properties = loadPropertiesFile();
+		final String driver=  properties.getProperty("driver");
+		final String url = properties.getProperty("url");
+		final String username = properties.getProperty("username");
+		final String password = properties.getProperty("passwaord");
+		Class.forName(driver);
+		Connection con =null;
+		con = DriverManager.getConnection(url,username,password);
+		if(con != null)
+			System.out.println("Connection Established");
+		return con;	
+		
+	}
+	public static Properties loadPropertiesFile() throws IOException 
+	{
+		Properties prop = new Properties();	
+		InputStream in = ConnectionManager.class.getClassLoader().getResourceAsStream("jdbc.properties");
+		prop.load(in);
+		in.close(); 
+		return prop;
+	}
 }
